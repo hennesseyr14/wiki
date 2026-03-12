f48af5174a840a3beedfd70cb47a9273ce41660a
hennesseyr14 <hennesseyr14@gmail.com>
2026-03-11 20:49:19 -0400

    404 if entry page is not found

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from . import util
@@ -10,7 +11,11 @@ def index(request):
 
 
 def wiki(request, title):
+    entry = util.get_entry(title)
+    if entry is None:
+        raise Http404("Entry not found")
+
     return render(request, "encyclopedia/wiki.html", {
         "title": title,
-        "entry": util.get_entry(title)
+        "entry": entry
     })
